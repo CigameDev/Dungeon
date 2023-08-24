743eaccbf1e4f2957f046025ba2094aa04537b82
Hoang Minh Chien <chienhm@apero.vn>
2023-08-24 23:29:18 +0700

    Update nhanh master

---
+++ Assets/_Script/Enum.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum StateGame
+{
+    Victory,
+    Gameover,
+    Pause,
+}
+public enum Direction
+{
+    Up,
+    Down,
+    Left,
+    Right,
+}
+
--- Assets/_Script/GameManager.cs
+++ Assets/_Script/GameManager.cs
@@ -13,7 +13,7 @@ public class GameManager : MonoBehaviour
     private void Start()
     {
         //viet linh tinh vao day
-        //ahfhsdf
+        //ahfhs
         //dsdfhsdfwuefe
     }
 
